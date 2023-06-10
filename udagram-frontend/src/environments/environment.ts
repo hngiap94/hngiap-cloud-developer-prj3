a6c68f31293e5d0bacbd5c58e57ef9b20dd097cc
hngiap <hngiap94@gmail.com>
2023-06-11 00:47:46 +0700

    update

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4891eb580d384a0f8f1b87d355cc5b5-1684404653.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
